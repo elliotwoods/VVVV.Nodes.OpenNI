376878d2b384672b726e43ce782113a9927c6936
Elliot Woods <elliot.woods@gmail.com>
2011-11-21 14:22:14 +0900

    updates to user node

---
+++ OpenNI/ContextNode.cs
@@ -0,0 +1,167 @@
+﻿#region usings
+using System;
+using System.ComponentModel.Composition;
+using System.Drawing;
+using System.Threading;
+
+using VVVV.PluginInterfaces.V1;
+using VVVV.PluginInterfaces.V2;
+using VVVV.Utils.VMath;
+using VVVV.Core.Logging;
+
+using ThreadState = System.Threading.ThreadState;
+using System.Collections.Generic;
+
+using OpenNI;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+using VVVV.Nodes.EmguCV;
+
+#endregion usings
+
+namespace VVVV.Nodes.OpenNI
+{
+
+	class OpenNIState {
+		public Context Context;
+		/// <summary>
+		/// We have a special instance here since we
+		/// want the thread to always wait on this
+		/// generator.
+		/// </summary>
+		public DepthGenerator DepthGenerator;
+		public bool Running = false;
+
+		public event EventHandler Initialised;
+		public void OnInitialised()
+		{
+			if (Initialised == null)
+				return;
+			Initialised(this, EventArgs.Empty);
+		}
+
+		public event EventHandler Update;
+		public void OnUpdate()
+		{
+			if (Update == null)
+				return;
+			Update(this, EventArgs.Empty);
+		}
+	}
+
+	#region PluginInfo
+	[PluginInfo(Name = "Context", Category = "OpenNI", Help = "OpenNI context loader", Tags = "")]
+	#endregion PluginInfo
+	public class ContextNode : IPluginEvaluate, IDisposable
+	{
+		#region fields & pins
+		[Input("Open", IsBang = true, IsSingle = true)]
+		ISpread<bool> FPinInOpen;
+
+		[Output("Context")]
+		ISpread<OpenNIState> FPinOutContext;
+
+		[Output("Status")]
+		ISpread<String> FPinOutStatus;
+
+		[Import]
+		ILogger FLogger;
+
+		string FStatus = "";
+
+		OpenNIState FState = new OpenNIState();
+		License FLicense = new License();
+		Thread FThread;
+		#endregion fields & pins
+
+		[ImportingConstructor]
+		public ContextNode(IPluginHost host)
+		{
+
+		}
+
+		public void Dispose()
+		{
+			Close();
+		}
+
+		//called when data for any output pin is requested
+		public void Evaluate(int SpreadMax)
+		{
+			if (FPinInOpen[0])
+			{
+				Open();
+			}
+
+			FPinOutContext[0] = FState;
+			FPinOutStatus[0] = FStatus;
+		}
+
+		void Open()
+		{
+			try
+			{
+				Close();
+
+				FLicense.Vendor = "PrimeSense";
+				FLicense.Key = "0KOIk2JeIBYClPWVnMoRKn5cdY4";
+
+				FState.Context = new Context();
+				FState.Context.AddLicense(FLicense);
+				FState.Context.GlobalMirror = false;
+
+				FState.DepthGenerator = new DepthGenerator(FState.Context);
+				FState.DepthGenerator.StartGenerating();
+
+				FState.OnInitialised();
+
+				FState.Running = true;
+				FThread = new Thread(ThreadedFunction);
+				FThread.Start();
+
+				FStatus = "OK";
+			}
+			catch (Exception e)
+			{
+				Close();
+				FStatus = e.Message;
+			}
+		}
+
+		void Close()
+		{
+			if (FState.Running)
+			{
+				if (FState.Context != null)
+				{
+					FState.Context.Release();
+				}
+
+				if (FThread != null)
+				{
+					FState.Running = false;
+					FThread.Join();
+				}
+			}
+		}
+
+		void ThreadedFunction()
+		{
+			while (FState.Running)
+			{
+				try
+				{
+					FState.Context.WaitOneUpdateAll(FState.DepthGenerator);
+					FState.OnUpdate();
+					FStatus = "OK";
+				}
+				catch (Exception e)
+				{
+					FStatus = e.Message;
+				}
+			}
+		}
+
+	}
+}
---
+++ OpenNI/ImageNode.cs
@@ -0,0 +1,211 @@
+﻿#region usings
+using System;
+using System.ComponentModel.Composition;
+using System.Drawing;
+using System.Threading;
+
+using VVVV.PluginInterfaces.V1;
+using VVVV.PluginInterfaces.V2;
+using VVVV.Utils.VMath;
+using VVVV.Core.Logging;
+
+using ThreadState = System.Threading.ThreadState;
+using System.Collections.Generic;
+
+using OpenNI;
+using System.Runtime.InteropServices;
+
+using Emgu.Util;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+using VVVV.Nodes.EmguCV;
+
+#endregion usings
+
+namespace VVVV.Nodes.OpenNI
+{
+	enum ImageNodeMode { RGB, IR };
+
+	#region PluginInfo
+	[PluginInfo(Name = "Images", Category = "OpenNI", Help = "OpenNI Image generator", Tags = "")]
+	#endregion PluginInfo
+	public class ImageNode : IPluginEvaluate, IDisposable
+	{
+		[DllImport("msvcrt.dll", EntryPoint = "memcpy")]
+		public unsafe static extern void CopyMemory(IntPtr pDest, IntPtr pSrc, int length);
+
+		#region fields & pins
+		[Input("Context", IsSingle=true)]
+		ISpread<OpenNIState> FPinInContext;
+
+		[Input("Mode", IsSingle = true)]
+		ISpread<ImageNodeMode> FPinInMode;
+
+		[Output("Image")]
+		ISpread<CVImageLink> FPinOutImageImage;
+
+		[Output("Depth")]
+		ISpread<CVImageLink> FPinOutImageDepth;
+
+		[Output("World")]
+		ISpread<CVImageLink> FPinOutImageWorld;
+
+		[Output("Status")]
+		ISpread<String> FPinOutStatus;
+
+		CVImageOutput FImageImage = new CVImageOutput();
+		CVImageOutput FImageDepth = new CVImageOutput();
+		CVImageOutput FImageWorld = new CVImageOutput();
+
+		ImageNodeMode FMode;
+
+		Point3D[] FProjective = new Point3D[640 * 480];
+		[Import]
+		ILogger FLogger;
+
+		OpenNIState FState;
+		ImageGenerator FImageGenerator;
+		IRGenerator FIRGenerator;
+
+		string FStatus = "";
+		#endregion fields & pins
+
+		[ImportingConstructor]
+		public ImageNode(IPluginHost host)
+		{
+
+		}
+
+		public void Dispose()
+		{
+
+		}
+
+		//called when data for any output pin is requested
+		public void Evaluate(int SpreadMax)
+		{
+			if (FPinInContext[0] != FState)
+			{
+				FState = FPinInContext[0];
+
+				if (FState != null)
+				{
+					//we've made a connection
+
+					//add a listener for intialisation
+					FState.Initialised += new EventHandler(FState_Initialise);
+
+					//if already initialised before we connected
+					if (FState.Running)
+						Initialise();
+				}
+			}
+
+			FPinOutStatus[0] = FStatus;
+		}
+
+		void FState_Initialise(object sender, EventArgs e)
+		{
+			Initialise();
+		}
+
+		private void Initialise()
+		{
+			try
+			{
+				FMode = FPinInMode[0];
+				Size size = new Size(640, 480);
+
+				if (FMode == ImageNodeMode.RGB)
+				{
+					FImageGenerator = new ImageGenerator(FState.Context);
+					FImageImage.Image.Initialise(size, TColourFormat.RGB8);
+					FState.DepthGenerator.AlternativeViewpointCapability.SetViewpoint(FImageGenerator);
+					FImageGenerator.StartGenerating();
+				}
+				else
+				{
+					FIRGenerator = new IRGenerator(FState.Context);
+					FImageImage.Image.Initialise(size, TColourFormat.L16);
+					FIRGenerator.StartGenerating();
+				}
+				
+				FImageDepth.Image.Initialise(size, TColourFormat.L16);
+				FImageWorld.Image.Initialise(size, TColourFormat.RGB32F);
+
+				FPinOutImageImage[0] = FImageImage.Link;
+				FPinOutImageDepth[0] = FImageDepth.Link;
+				FPinOutImageWorld[0] = FImageWorld.Link;
+
+				for (int x = 0; x < 640; x++)
+					for (int y = 0; y < 480; y++)
+					{
+						FProjective[x + y * 640].X = x;
+						FProjective[x + y * 640].Y = y;
+					}
+
+				FState.Update += new EventHandler(FState_Update);
+
+				FStatus = "OK";
+			}
+			catch (StatusException e)
+			{
+				FStatus = e.Message;
+			}
+		}
+
+		void FState_Update(object sender, EventArgs e)
+		{
+			Update();
+		}
+
+		private unsafe void Update()
+		{
+			if (FMode == ImageNodeMode.RGB)
+			{
+				byte* rgbs = (byte*)FImageGenerator.ImageMapPtr.ToPointer();
+				byte* rgbd = (byte*)FImageImage.Image.Data.ToPointer();
+
+				for (int i = 0; i < 640 * 480; i++)
+				{
+					rgbd[2] = rgbs[0];
+					rgbd[1] = rgbs[1];
+					rgbd[0] = rgbs[2];
+					rgbs += 3;
+					rgbd += 3;
+				}
+			}
+			else if (FMode == ImageNodeMode.IR)
+			{
+				FImageImage.Image.SetPixels(FIRGenerator.IRMapPtr);
+			}
+
+			FImageDepth.Image.SetPixels(FState.DepthGenerator.DepthMapPtr);
+
+			fillWorld();
+
+			FImageImage.Send();
+			FImageDepth.Send();
+			FImageWorld.Send();
+		}
+
+		private unsafe void fillWorld()
+		{
+			float* xyz = (float*)FImageWorld.Data.ToPointer();
+			ushort* d = (ushort*)FImageDepth.Data.ToPointer();
+
+			for (int i = 0; i < 640*480; ++i)
+				FProjective[i].Z = *d++;
+
+			Point3D[] xyzp = FState.DepthGenerator.ConvertProjectiveToRealWorld(FProjective);
+
+			for (int i = 0; i < 640 * 480; ++i, xyz += 3)
+			{
+				xyz[0] = xyzp[i].X / 1000.0f;
+				xyz[1] = xyzp[i].Y / 1000.0f;
+				xyz[2] = xyzp[i].Z / 1000.0f;
+			}
+		}
+	}
+}
